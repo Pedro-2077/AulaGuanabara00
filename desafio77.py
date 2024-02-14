7e4e5de014a86758c270180dfa5677114645d093
alpacah <pedronit2016@gmail.com>
2024-02-14 18:53:50 -0300

    Python-Desafios e aulas

--- desafio77.py
+++ desafio77.py
@@ -3,12 +3,12 @@ print('===== DESAFIO 77 =====')
 palavra = (str(input('Digite uma palavra: ')),str(input('Digite uma palavra: ')),
            str(input('Digite uma palavra: ')), str(input('Digite uma palavra: ')))
 
-for letra in range(0, len(palavra)):
+"""for letra in range(0, len(palavra)):
     print(f'\n{palavra[letra]} = ', end='')
     for letra2 in range(0, len(palavra[letra])):
         if palavra [letra][letra2] in 'aAeEeIiOoUuUu':
             print(f'{palavra[letra][letra2]}', end=' ')
-
+"""
 #MODO GUANABARA
 
 for letra in palavra:
