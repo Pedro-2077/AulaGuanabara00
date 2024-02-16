d922dd7c835c834d51c233669cdd5887951da8a6
alpacah <pedronit2016@gmail.com>
2024-02-16 13:22:32 -0300

    Python-Desafios e aulas

--- desafio88.py
+++ desafio88.py
@@ -12,16 +12,16 @@ for c in range(0, qtd):
     for d in range(0, 1):
         dado = (random.randint(1, 60), random.randint(1, 60), random.randint(1, 60)
                 , random.randint(1, 60), random.randint(1, 60), random.randint(1, 60))
+
         jogos.append(dado)
     del dado
 
-jogos.sort()
 print('=-'*30)
 print(f'Matriz do game:{jogos}')
 print('=-'*30)
 
 for pos in range(0, qtd):
-    print(f'O jogo {pos+1} é = {jogos[pos]} ')
+    print(f'O jogo {pos+1} é = {sorted(jogos[pos])} ')
     sleep(1)
 
 
