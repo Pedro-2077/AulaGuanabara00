c13392e02543afb2708b8616f1fc078626078cd3
alpacah <pedronit2016@gmail.com>
2024-02-18 13:54:27 -0300

    Python-Desafios e aulas

--- desafio93.py
+++ desafio93.py
@@ -8,19 +8,18 @@ jogador['nome'] = str(input('Nome do Jogado:'))
 n = int(input(f'Quantas partidas o {jogador["nome"]} jogou? '))
 
 for c in range(0, n):
-    dadoGol.append(int(input(f'Quantos gols na partida {n}?:')))
+    dadoGol.append(int(input(f'Quantos gols na partida {c}?:')))
     soma = soma + dadoGol[c]
 
-jogador['Gosl'] = dadoGol
-jogador['total'] = soma
+jogador['Gosl'] = dadoGol[:]
+jogador['total'] = soma #JOGADOR['total'] = SUM(DADOSGOL)
 
 print('=-'*30)
 print(jogador)
 print('=-'*30)
 
-print(f'O campo nome tem o valor {jogador['nome']}')
-print(f'O campo gols tem o valor {dadoGol}')
-print(f'O campo total de {jogador['total']} gols')
+for k, v in jogador.items():
+    print(f'O campo {k} tem o valor {v}')
 
 print('=-'*30)
 print(f'O jogador {jogador['nome']} jogou {n}:')
